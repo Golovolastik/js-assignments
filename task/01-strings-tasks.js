249be0516897e23e5c16c22cdece5c8dc9d3a911
Golovolastik <yankovich.aliaksei@gmail.com>
2023-03-18 17:11:32 +0300

    complete 13 tasks

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -224,7 +224,26 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-    throw new Error('Not implemented');
+    let arr = str.split('');
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i].charCodeAt(0) >= 65 && arr[i].charCodeAt(0) <= 90){
+            if (arr[i].charCodeAt(0) + 13 <= 90){
+                arr[i] = String.fromCharCode(arr[i].charCodeAt(0) + 13);
+            }
+            else {
+                arr[i] = String.fromCharCode(arr[i].charCodeAt(0) - 13)
+            }
+        }
+        else if (arr[i].charCodeAt(0) >= 97 && arr[i].charCodeAt(0) <= 122){
+            if (arr[i].charCodeAt(0) + 13 <= 122){
+                arr[i] = String.fromCharCode(arr[i].charCodeAt(0) + 13);
+            }
+            else {
+                arr[i] = String.fromCharCode(arr[i].charCodeAt(0) - 13)
+            }
+        }
+    }
+    return arr.join('')
 }
 
 /**
